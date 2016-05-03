a45e96117ac0af1cf7ac9808a230cf621fe5f30a
Thomas Venries <thomas.venries@gmail.com>
2016-05-03 20:21:03 +0200

    src/: correct cfan.c syntax according to checkpatch.pl

--- src/cfan.c
+++ src/cfan.c
@@ -16,38 +16,31 @@ MODULE_LICENSE("GPL");
 #define VENDOR_ID	0x0c45
 #define PRODUCT_ID	0x7701
 
-// Private structure
 struct usb_cfan {
 	struct usb_device	*udev;
 	unsigned char		bmp[39][8];
 	unsigned int		cfg[8];
 };
 
-// Forward declaration
 static struct usb_driver cfan_driver;
 
-/* Table of devices that work with this driver.
- * Informations for the Linux-Hotplug system to autoload the driver.
- */
-static struct usb_device_id id_table [] = {
+/* Add the device to this driver's supported device list */
+static struct usb_device_id id_table[] = {
 	{ USB_DEVICE(VENDOR_ID, PRODUCT_ID) },
 	{ },
 };
-MODULE_DEVICE_TABLE (usb, id_table);
+MODULE_DEVICE_TABLE(usb, id_table);
 
-// USB driver functions
-static int cfan_probe (struct usb_interface *interface,
-			const struct usb_device_id *id)
+static int cfan_probe(struct usb_interface *interface,
+		      const struct usb_device_id *id)
 {
-  printk (KERN_INFO "%s: USB Cheeky Fan has been connected\n",
-		__FUNCTION__);
-  return 0;
+	pr_info("%s: USB Cheeky Fan has been connected\n", __func__);
+	return 0;
 }
 
 static void cfan_disconnect(struct usb_interface *interface)
 {
-	printk (KERN_INFO "%s: USB Cheeky Fan has been disconnected.\n",
-		__FUNCTION__);
+	pr_info("%s: USB Cheeky Fan has been disconnected.\n", __func__);
 }
 
 static struct usb_driver cfan_driver = {
@@ -66,7 +59,7 @@ static int __init usb_cfan_init(void)
 static void __exit usb_cfan_exit(void)
 {
 	/* deregister this driver with the USB subsystem */
- 	usb_deregister(&cfan_driver);
+	usb_deregister(&cfan_driver);
 }
 
 module_init(usb_cfan_init);
