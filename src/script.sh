83275c18b8c29b1bf33aa3c34760f9bbc1c91829
Thomas Venries <thomas.venries@gmail.com>
2016-05-08 05:53:05 +0200

    script.sh: modify indent according to checkpatch
    Signed-off-by: Thomas Venries <thomas.venries@gmail.com>

--- src/script.sh
+++ src/script.sh
@@ -7,33 +7,33 @@ ID_PRODUCT="7701"
 
 # Creation of the udev rule file
 if [ ! -e "$RULE" ] ; then
-    echo -n "ATTRS{idVendor}==\"$ID_VENDOR\", " >> $RULE;
-    echo "ATTRS{idProduct}==\"$ID_PRODUCT\", \\" >> $RULE;
-    echo -n "PROGRAM=\"/bin/sh -c " >> $RULE;
-    echo "'echo -n \$id:1.0 >/sys/bus/usb/drivers/usbhid/unbind;\\" >> $RULE;
-    echo "echo -n \$id:1.0 > /sys/bus/usb/drivers/$MODULE/bind'\"" >> $RULE
+	echo -n "ATTRS{idVendor}==\"$ID_VENDOR\", " >> $RULE;
+	echo "ATTRS{idProduct}==\"$ID_PRODUCT\", \\" >> $RULE;
+	echo -n "PROGRAM=\"/bin/sh -c " >> $RULE;
+	echo "'echo -n \$id:1.0 >/sys/bus/usb/drivers/usbhid/unbind;\\" >> $RULE;
+	echo "echo -n \$id:1.0 > /sys/bus/usb/drivers/$MODULE/bind'\"" >> $RULE
 fi
 
 # Build the module
 make clean && make && \
-    echo "\n-->Make '$MODULE' has been done."
+	echo "\n--> Make '$MODULE' has been done."
 
 # Reset the module
 if [ ! -z "$(lsmod | grep $MODULE)" ] ; then
-    sudo rmmod $MODULE && \
-    echo "--> Module '$MODULE' has been removed."
+	sudo rmmod $MODULE && \
+		echo "--> Module '$MODULE' has been removed."
 else
-    sudo insmod $MODULE\.ko && \
-        echo "--> Module '$MODULE' has been added."
+	sudo insmod $MODULE\.ko && \
+		echo "--> Module '$MODULE' has been added."
 fi
 
 # Add the rule if it is not exist
 if [ -z `ls /etc/udev/rules.d/ | grep $RULE` ] ; then
-    sudo cp 99-cfan.rules /etc/udev/rules.d/ && \
-        sudo udevadm control --reload-rules && \
-        echo "--> Rule '$RULE' has been added."
+	sudo cp 99-cfan.rules /etc/udev/rules.d/ && \
+		sudo udevadm control --reload-rules && \
+		echo "--> Rule '$RULE' has been added."
 else
-    echo "--> Rule '$RULE' has already been added."
+	echo "--> Rule '$RULE' has already been added."
 fi
 
 # Tests
