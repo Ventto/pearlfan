fd3a02dc0b321cfdbbfc7fd505ad1e14fb8329dd
Thomas Venries <thomas.venries@gmail.com>
2016-05-12 10:51:35 +0200

    tests/: write 'APCDD' with config
    Signed-off-by: Thomas Venries <thomas.venries@gmail.com>

--- tests/test.c
+++ tests/test.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <string.h>
+#include <errno.h>
 
 #define DEVICE		("/dev/cfan0")
 
@@ -13,13 +14,46 @@ int main(int argc, char **argv)
 	fd = open(DEVICE, O_WRONLY);
 
 	if (fd <= 0)
-		return 2;
+		return -EBADF;
 
-	if (!write(fd, "hello", 5)) {
+	unsigned char *buffer = NULL;
+
+	/* Buffer:
+	 * - Number of displays : 1 bytes
+	 * - Configuration : 2 bytes,
+	 * - Display : 26 bytes max (letters)
+	 */
+	buffer = malloc(6);
+
+	if (!buffer)
+		return -ENOMEM;
+
+	/* Configuration:
+	 * 00 00 00 55 [C][D] [A][B] 10 A0
+	 * [A] : OpenOption	[0 ; 5]
+	 * [B] : CloseOption	[0 ; 5]
+	 * [C] : TurnOption	(6 | c)
+	 * [D] : Display ID	[0 ; 7]
+	 * Warning: Reverse bytes sequence (stack behind)
+	 */
+	buffer[0] = 0x01; /* Number of displays */
+	buffer[1] = 0x44; /* Open & Close options */
+	buffer[2] = 0x00; /* Turn option & Display ID */
+
+	buffer[3] = 'A';
+	buffer[4] = 'P';
+	buffer[5] = 'C';
+	buffer[6] = 'D';
+	buffer[7] = 'D';
+	buffer[8] = '\0';
+
+	if (!write(fd, buffer, 9)) {
 		close(fd);
-		return 3;
+		free(buffer);
+		return -ENOSPC;
 	}
 
+	free(buffer);
 	close(fd);
 
 	return EXIT_SUCCESS;
