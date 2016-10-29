84d9eedba540e3e650173d23fe16b92ab88f77dd
Thomas Venries <thomas.venries@gmail.com>
2016-10-30 01:12:27 +0200

    libusb: Factorize sending loops

--- src/libusb/main.c
+++ src/libusb/main.c
@@ -14,18 +14,6 @@
 #define FAN_MAX_IMG_NBR			8
 #define FAN_MAX_DATA			(FAN_MAX_IMG_NBR * 2 * 156)
 
-static uint64_t effect_to_usbdata(const char id, const char effect[3])
-{
-	uint16_t opts = 0;
-
-	opts |= effect[CLOSING_EFFECT];
-	opts |= (effect[OPENING_EFFECT] << 4);
-	opts |= (id << 8);
-	opts |= (effect[DISPLAY_EFFECT] << 12);
-
-	return 0x00000055000010A0 | (opts << 16);
-}
-
 int main(int argc, char **argv)
 {
 	if (argc != 2) {
@@ -42,22 +30,22 @@ int main(int argc, char **argv)
 
 	char imgs[MAX_FAN_DISPLAYS][FILEPATH_MAX];
 	char effects[MAX_FAN_DISPLAYS][3];
-	int img_nbr;
+	int img_n;
 
-	if ((img_nbr = pfan_read_config(config_file, imgs, effects)) < 0) {
+	if ((img_n = pfan_read_config(config_file, imgs, effects)) < 0) {
 		printf("pfan: '%s' invalid config file.\n", config_file);
 		return EXIT_FAILURE;
 	}
 
-	if (!img_nbr)
+	if (!img_n)
 		return EXIT_SUCCESS;
 
 	FILE *img = NULL;
-	bit **rasters = malloc(sizeof(void *) * img_nbr);
+	bit **rasters = malloc(sizeof(void *) * img_n);
 
 	pm_init(argv[0], 0);
 
-	for (int i = 0; i < img_nbr; i++) {
+	for (int i = 0; i < img_n; i++) {
 		img = pm_openr(imgs[i]);
 		if (!img) {
 			printf("pfan: can not open '%s'\n", imgs[i]);
@@ -67,39 +55,33 @@ int main(int argc, char **argv)
 		pm_close(img);
 	}
 
-	uint16_t fan_displays[FAN_MAX_IMG_NBR][156];
-	memset(fan_displays, 0xFF, FAN_MAX_DATA);
+	uint16_t displays[FAN_MAX_IMG_NBR][156];
+	memset(displays, 0xFF, FAN_MAX_DATA);
 
-	for (int i = 0; i < img_nbr ; i++)
-		pfan_convert_raster(i, rasters[i], fan_displays[i]);
+	for (int i = 0; i < img_n ; i++)
+		pfan_convert_raster(i, rasters[i], displays[i]);
 
 	libusb_device_handle *usb_handle = NULL;
 	libusb_context *usb_ctx = NULL;
 
 	if (libusb_init(&usb_ctx) < 0) {
-		pfan_free_rasters(rasters, img_nbr);
+		pfan_free_rasters(rasters, img_n);
 		printf("pfan: libusb initialization failed.\n");
 		return EXIT_FAILURE;
 	}
 
 	usb_handle = pfan_open(usb_ctx, VENDOR_ID, PRODUCT_ID);
 	if (!usb_handle) {
-		pfan_free_rasters(rasters, img_nbr);
+		pfan_free_rasters(rasters, img_n);
 		libusb_exit(usb_ctx);
 		return EXIT_FAILURE;
 	}
 
-	for (uint8_t i = 0; i < img_nbr; i++) {
-		uint64_t effect = effect_to_usbdata(i, effects[i]);
-
-		pfan_send(usb_handle, &effect);
-		for (uint8_t j = 0; j < 39; ++j)
-			pfan_send(usb_handle, &fan_displays[i][j * 4]);
-	}
+	pfan_send_all(usb_handle, img_n, effects, displays);
 
 	libusb_close(usb_handle);
 	libusb_exit(usb_ctx);
-	pfan_free_rasters(rasters, img_nbr);
+	pfan_free_rasters(rasters, img_n);
 
 	return EXIT_SUCCESS;
 }
--- src/libusb/usb.h
+++ src/libusb/usb.h
@@ -5,6 +5,8 @@
 
 libusb_device_handle *pfan_open(libusb_context *ctx, int vid, int pid);
 
-int pfan_send(libusb_device_handle *dev_handle, void *data);
+int pfan_send_all(libusb_device_handle *dev_handle, int img_n,
+		char effects[8][3],
+		uint16_t displays[8][156]);
 
 #endif /* !USB_H_ */
