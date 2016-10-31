8e889dfc49f87d80c7ab4ea2f7e27e801cd862f3
Thomas Venries <thomas.venries@gmail.com>
2016-10-31 18:38:33 +0100

    misc: Use mask directly into the convert func

--- src/common/convert.c
+++ src/common/convert.c
@@ -5,10 +5,9 @@
 #endif
 
 #include "convert.h"
-#include "mask.h"
 
-#define IMAGE_WIDTH	156
-#define IMAGE_HEIGHT	11
+#define IMG_WIDTH     156
+#define IMG_HEIGHT    11
 
 unsigned long long pfan_convert_effect(const char id, const char effect[3])
 {
@@ -29,13 +28,17 @@ void pfan_convert_raster(char id, unsigned char *raster,
 	unsigned char col_end;
 	unsigned char i;
 	unsigned char j;
+	unsigned short mask[] = {
+		0xFCFF, 0xFBFF, 0xF7FF, 0xFEFF, 0xCFFF, 0xBFFF,
+		0x7FFF, 0xFFFE, 0xFFFD, 0xFFFB, 0xFFF7
+	};
 
-	for (i = 0; i < IMAGE_WIDTH; ++i) {
-		col_end = IMAGE_WIDTH - i - 1;
-		for (j = 0; j < IMAGE_HEIGHT; ++j)
-			if (raster[j * IMAGE_WIDTH + i] == 1)
+	for (i = 0; i < IMG_WIDTH; ++i) {
+		col_end = IMG_WIDTH - i - 1;
+		for (j = 0; j < IMG_HEIGHT; ++j)
+			if (raster[j * IMG_WIDTH + i] == 1)
 				display[col_end] &=
-					pfan_mask[IMAGE_HEIGHT - j - 1];
+					mask[IMG_HEIGHT - j - 1];
 	}
 }
 EXPORT_SYMBOL(pfan_convert_raster);
