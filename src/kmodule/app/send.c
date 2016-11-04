9504b739bd9e632b36d428a7b92f743f3690cb77
Thomas Venries <thomas.venries@gmail.com>
2016-11-04 19:21:55 +0100

    misc: Add LICENSE for each src-file

--- src/kmodule/app/send.c
+++ src/kmodule/app/send.c
@@ -1,3 +1,21 @@
+/*
+ * The GPL License (GPLv3)
+ *
+ * Copyright © 2016 Thomas "Ventto" Venriès  <thomas.venries@gmail.com>
+ *
+ * Pearlfan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pearlfan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Pearlfan.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
